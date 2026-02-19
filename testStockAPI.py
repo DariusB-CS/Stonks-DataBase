724e24dedc6d010e1d8c1e3d14e608e1b2cc5b84
Amaar Sharif <nuclear@Amaars-MacBook-Air.local>
2026-02-18 22:03:53 -0600

    Ok so I did a good bit for this commit here 1. I changed the website that we get the information from to just be the S&P 500 instead of the 12,000 tickers I had before 2. I used the library beautiful soup to parse the website to get only the table 3. I then went through a for loop to get each ticker/symbol into the ticker list 4. I removed all the newline characters from the tickers 5. I then actually got all the ticker information from the yfinance thingie 6. I restructered the data to make it look better 7. I then just made it into a csv for my own brains sake ---Error handling if this doesn't work for you -Make sure you download all the libraries I imported -You might have to change your own headers variable to be your specifc browsers user agent -Everything else should work fine I've commented out all my failed attempts, I'm just keeping it just in case my solution actually fucks up ---Next steps -I am going to figure out how to now put all this information into a sql database

--- testStockAPI.py
+++ testStockAPI.py
@@ -2,33 +2,51 @@ import yfinance as yf
 import requests
 import pandas as pd
 import mysql.connector
+import bs4 as bs
 
-def get_alpha_vantage_symbols(api_key): # Gets all the tickers from a website might have to figure out how to limit it
-    url = f'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={api_key}'
+#def get_alpha_vantage_symbols(api_key): # Gets all the tickers from a website might have to figure out how to limit it
+ #   url = f'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={api_key}'
     
     # Download CSV data
-    with requests.get(url) as r:
-        data = r.content.decode('utf-8').split('\n')
-        # Process and store symbols
-        symbols = [line.split(',')[0] for line in data[1:] if line]
-        return symbols
+  #  with requests.get(url) as r:
+   #     data = r.content.decode('utf-8').split('\n')
+    #    # Process and store symbols
+     #   symbols = [line.split(',')[0] for line in data[1:] if line]
+      #  return symbols
+#tickers = get_alpha_vantage_symbols("PI5qAc7UFswiigQSOXmVmT6haKhG0it4")
+#data = pd.DataFrame(columns=tickers) # Uses a pandas dataframe to hold all the symbol data
+#for ticker in tickers: # Gets the ticker data from yfinance but I think it's too many tickers
+ #   data[ticker] = yf.download(ticker, auto_adjust=False)['Adj Close']
 
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15 Ddg/18.6'} # Make sure you change the user agent to fit your browser
+resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies' , headers=headers) # Get the website information from wikipedia
+soup = bs.BeautifulSoup(resp.text, 'lxml') # Parse it using beautiful soup to get the html information
+#table = soup.find('table', attrs={'class': 'wikitable sortable', 'id': 'constituents'}) # Find the table that has the S&P 500 tickers
+#table = soup.find('table', {'class': 'wikitable sortable'})
+table = soup.find('table') # Find the table that has the S&P 500 tickers
 
+tickers = []
 
+for row in table.find_all('tr')[1:]: # tr is the table row and you skip that first row
+    ticker = row.find_all('td')[0].text # td [0] gives you the first column which is the ticker name
+    tickers.append(ticker) 
+
+tickers = [s.replace('\n', '') for s in tickers] # Remove the newline character from each ticker name
+
+data = yf.download(tickers, period ='1d', auto_adjust = False) # Get the full ticker information from yfinance
+df = data.stack().reset_index().rename(index=str, columns={"level_1": "Ticker"}).sort_values(['Ticker','Date']) # Trying to make it look better and more organized
+df.set_index('Date', inplace=True)
+
+print(df.head()) # Gets the tail end of all the data
+df.to_csv('sp500_stocks.csv', mode='w+')
 # Connecting from the server
-conn = mysql.connector.connect(user = 'root', # Connects to mydatabase
-                               host = 'localhost',
-                               passwd = 'YOURPASSWORD',
-                              database = 'STONKS')
-print(conn)
 
-tickers = get_alpha_vantage_symbols("PI5qAc7UFswiigQSOXmVmT6haKhG0it4")
-data = pd.DataFrame(columns=tickers) # Uses a pandas dataframe to hold all the symbol data
-
-for ticker in tickers: # Gets the ticker data from yfinance but I think it's too many tickers
-    data[ticker] = yf.download(ticker, auto_adjust=False)['Adj Close']
-
-data.tail() # Gets the tail end of all the data
+#conn = mysql.connector.connect(user = 'root', # Connects to mydatabase
+ #                              host = 'localhost',
+  #                             passwd = 'YOURPASSWORD',
+   #                           database = 'STONKS')
+#print(conn)
 
 # Disconnecting from the server
-conn.close()
+#conn.close()
+
